6fd4176493f14485a6367095232eadacb562743b
reinosobs <sergio.reinosobarrios@betssongroup.com>
2023-08-17 21:02:10 +0200

    portfolio working

---
+++ src/components/Resume/index.js
@@ -0,0 +1,149 @@
+import BarGraph from "./BarGraph";
+import DownloadIcon from "@mui/icons-material/Download";
+
+export default function Resume() {
+  return (
+    <div id="resume" className="min-h-screen flex-col text-center">
+      <div className="text-5xl font-bold py-8" data-aos={"slide-down"}>
+        Resume
+      </div>
+      <div className="grid grid-cols-2 gap-7 mt-7">
+        <ol class="relative border-l  border-gray-700 ml-9 mb-6">
+          <li class="mb-10" data-aos="zoom-in">
+            <span class="absolute flex items-center justify-center w-6 h-6   rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3  text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              Full-stack Developer - Betsson Group
+            </h3>
+            <time class="block pt-1 pb-3  text-sm font-normal leading-none  text-gray-500">
+              Nov 2021 - Present
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+              Proficiently contribute to end-to-end web solution lifecycle encompassing analysis, design, development, testing, release, 
+              administration, and implementation. Skillfully craft robust and reusable code while fostering integrations and prototypes 
+              for diverse platforms, including cloud systems and external interfaces. Drive maintenance, expansion, and enhancement of
+              Betsson Group's digital assets. Ensure application quality and offer adept technical support,adeptly troubleshoot issues,
+              and continually enhance platform architecture in alignment with evolving technology trends.
+            </p>
+          </li>
+          <li class="mb-10" data-aos="zoom-out" data-aos-delay="700">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              Junior Full-stack Developer - URJC
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Feb 2021 - May 2021
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+              Learned how to write back end apps with PHP, Angular and npm. I also
+              built soem new tools to the web application already available.
+            </p>
+          </li>
+          <li data-aos="zoom-in" data-aos-offset={"-15"} data-aos-delay="1300">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              Oracle Database Administrator - URJC
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Feb 2019 - Feb 2021
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+              It was my first IT job at the University of Rey Juan Carlos which allowed me to learn about database management 
+              with MySQL Oracle. This helps me to develop a robust and scalable database system in my current projects.
+            </p>
+          </li>
+          <li data-aos="zoom-in" data-aos-offset={"-15"} data-aos-delay="1300">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              Customer Service Specialist - URJC
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Jan 2018 - Feb 2019
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+              Provide support and assistance to students, professors, and staff in various matters 
+              related to the institution's services and resources. 
+           </p>
+          </li>
+        </ol>
+
+        <div className="flex flex-col text-center" data-aos="slide-up">
+          <div className="text-3xl font-bold py-3">My Skills</div>
+          <div className="ml-16 flex flex-col gap-8 mt-6">
+            <BarGraph name={"PHP"} percent={90} />
+            <BarGraph name={"React"} percent={35} />
+            <BarGraph name={"TypeScript"} percent={50} />
+            <BarGraph name={"Java"} percent={80} />
+            <BarGraph name={"HTML/CSS"} percent={85} />
+            <BarGraph name={"SQL"} percent={90} />
+            <BarGraph name={"AWS"} percent={40} />
+            <BarGraph name={"VueJS"} percent={80} />
+          </div>
+
+          <a
+            href={require("../../assets/files/Sergio Resume.pdf")}
+            download={"Sergio's CV"}
+            class="inline-flex items-center px-6 py-2 mt-12 mx-auto w-fit text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+          >
+            Download CV <DownloadIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Alexa Skill",
+    desc: "In my final thesis, I developed a skill for the conversational agent Alexa. Without any prior knowledge of NodeJS and services offered by Alexa, such as the skill creation environment or AWS services, I worked and trained with several courses on skill creation and NodeJS programming.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "red-600",
+      },
+      {
+        name: "NextJs",
+        color: "green-600",
+      },
+    ],
+    img: "https://i.ibb.co/bXvYkqN/alexa.png",
+    link: "https://www.amazon.es/Universidad-Rey-Juan-Carlos-personal/dp/B08ZY11F1K/ref=sr_1_1?",
+    code: "https://github.com/reinosobs/agente-personal-alexa-skill",
+  },
+  {
+    title: "Gym-Web-App",
+    desc: "An app made with Java, Bootstrap and MySQL to help a gym communite to register and follow specific plans of food and training. It was a university project which wasn't never deployed.",
+    tags: [
+      {
+        name: "Java",
+        color: "red-600",
+      },
+      {
+        name: "Bootstrap",
+        color: "green-600",
+      },
+      {
+        name: "MySql",
+        color: "yellow-600",
+      },
+      {
+        name: "Docker",
+        color: "blue-600",
+      }
+    ],
+    img: "https://i.ibb.co/zfqJ0KW/website.jpg",
+    link: "https://sergio-reinoso.dev",
+    code: "https://github.com/reinosobs/ContactoCero",
+  },
+];
